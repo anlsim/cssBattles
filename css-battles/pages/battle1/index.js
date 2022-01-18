e991dbf2f67f73998bd683cf6909758214b58c9f
anlsim <anlsimental@gmail.com>
2022-01-17 21:33:01 -0800

    added nine

--- css-battles/pages/battle1/index.js
+++ css-battles/pages/battle1/index.js
@@ -2,17 +2,17 @@ import style from '../../styles/pages/_battle1.module.css';
 const battle1 = () => {
   return (
     <div className={style.main}>
-       <h1>Battle 1</h1>
-       <h2 className={style.title}>#1</h2>
-      <div className={style.bone}>
+      <h1>Battle 1</h1>
 
+      <h2 className={style.title}>#7</h2>
+      <div className={[style.container, style.bone].join(' ')}>
         <div className={[style.bottom, style.lemon].join(' ')}></div>
-        <div  className={[style.middle, style.lemon].join(' ')}></div>
-        <div  className={[style.top, style.lemon].join(' ') }></div>   
+        <div className={[style.middle, style.lemon].join(' ')}></div>
+        <div className={[style.top, style.lemon].join(' ') }></div>   
       </div>
-      <h2 className={style.title}>#2</h2>
-      <div className={style.btwo}>
 
+      <h2 className={style.title}>#8</h2>
+      <div className={[style.container, style.btwo].join(' ')}>
         <div className={style.circle}></div>
         <div className={[style.line1, style.dkLine].join(' ')}></div>
         <div className={[style.line2, style.dkLine].join(' ')}></div>
@@ -22,6 +22,13 @@ const battle1 = () => {
         <div className={[style.b, style.lgLine].join(' ')}></div>
         <div className={[style.c, style.lgLine].join(' ')}></div>
         <div class={style.handle}></div>
+      </div>
+      <h2 className={style.title}>#9</h2>
+      <div className={[style.container, style.bthree].join(' ')}>
+        <div className={style.rectangle}></div>
+        <div className={style.sqrA}></div>
+        <div className={style.sqrB}></div>
+        <div className={style.crcle}></div>
       </div>
     </div>
   )
