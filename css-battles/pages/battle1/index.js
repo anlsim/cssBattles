7f222f4e8a61922220cb4f41a843fd198a6ed072
anlsim <anlsimental@gmail.com>
2022-01-18 20:30:26 -0800

    three

--- css-battles/pages/battle1/index.js
+++ css-battles/pages/battle1/index.js
@@ -6,18 +6,30 @@ const battle1 = () => {
       <div className={style.main}>
     
       <div className={[style.container, style.buno].join(' ')}>
-      <h2>#1 - Simply Square</h2>
+        <h2>#1 - Simply Square</h2>
         <div className={style.square}></div>
       </div>
-      <div className={[style.container, style.bone].join(' ')}>
-      <h2>#7 - Leafy Trail</h2>
+
+      <div className={[style.container, style.btwo].join(' ')}>
+        <h2>#2 - Carro</h2>
+      </div>
+
+      <div className={[style.container, style.bthree].join(' ')}>
+        <h2>#3 - Push Button</h2>
+        <div className={style.aTwo}>
+            <div className={style.bTwo}></div>
+        </div>
+      </div>
+
+      <div className={[style.container, style.bseven].join(' ')}>
+        <h2>#7 - Leafy Trail</h2>
         <div className={[style.bottom, style.lemon].join(' ')}></div>
         <div className={[style.middle, style.lemon].join(' ')}></div>
         <div className={[style.top, style.lemon].join(' ') }></div>   
       </div>
 
-      <div className={[style.container, style.btwo].join(' ')}>
-      <h2>#8 - Forking Crazy</h2>
+      <div className={[style.container, style.beight].join(' ')}>
+        <h2>#8 - Forking Crazy</h2>
         <div className={style.circle}></div>
         <div className={[style.line1, style.dkLine].join(' ')}></div>
         <div className={[style.line2, style.dkLine].join(' ')}></div>
@@ -29,8 +41,8 @@ const battle1 = () => {
         <div class={style.handle}></div>
       </div>
 
-      <div className={[style.container, style.bthree].join(' ')}>
-      <h2>#9 Tesseract</h2>
+      <div className={[style.container, style.bnine].join(' ')}>
+        <h2>#9 Tesseract</h2>
         <div className={style.rectangle}></div>
         <div className={style.sqrA}></div>
         <div className={style.sqrB}></div>
@@ -48,6 +60,16 @@ const battle1 = () => {
         <div className={[style.AsmTen, style.smCircleTen ].join(' ')}></div>
         <div className={[style.BsmTen, style.smCircleTen ].join(' ')}></div>
         <div className={[style.CsmTen, style.smCircleTen ].join(' ')}></div>
+      </div>
+      
+      <div className={[style.container, style.beleven].join(' ')}>
+        <h2>#11 - Eye of Sauron</h2>
+        <div id="xEleven"></div>
+        <div id="yEleven"></div>
+        <div className={style.BGcircleEleven}></div>
+        <div className={style.MDcircleEleven}></div>
+        <div className={style.SDcircleEleven}></div>
+
       </div>
     </div>
     </>
